ae661afe0d8b9aaad5153229d40bc8a20d1e8106
Daniil Fedotov <165712645+dann-fe@users.noreply.github.com>
2024-08-05 18:11:32 +0200

    Add files via upload

---
+++ test_teenager.py
@@ -0,0 +1,6 @@
+age = int(input("your age is: "))
+
+if age >= 13 and age <= 19:
+    print("youre a teenager, because you're " + str(age))
+else:
+    print("you're not a teenager, because you're " + str(age))
