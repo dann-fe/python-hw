7c691301fa755074771f05a32bd0f5d86477fcab
Daniil Fedotov <165712645+dann-fe@users.noreply.github.com>
2024-08-06 23:43:32 +0200

    Delete test_teenager.py

--- test_teenager.py
+++
@@ -1,6 +0,0 @@
-age = int(input("your age is: "))
-
-if age >= 13 and age <= 19:
-    print("youre a teenager, because you're " + str(age))
-else:
-    print("you're not a teenager, because you're " + str(age))
